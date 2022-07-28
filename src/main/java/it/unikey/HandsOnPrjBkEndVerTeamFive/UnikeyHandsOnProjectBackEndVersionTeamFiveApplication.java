559ccc350ff1eaddec2aff41e29b13fbb45883f0
Federico D'Andrea <federico.dand@gmail.com>
2022-07-28 17:45:31 +0200

    initial commit

---
+++ src/main/java/it/unikey/HandsOnPrjBkEndVerTeamFive/UnikeyHandsOnProjectBackEndVersionTeamFiveApplication.java
@@ -0,0 +1,13 @@
+package it.unikey.HandsOnPrjBkEndVerTeamFive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnikeyHandsOnProjectBackEndVersionTeamFiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnikeyHandsOnProjectBackEndVersionTeamFiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unikey/HandsOnPrjBkEndVerTeamFive/UnikeyHandsOnProjectBackEndVersionTeamFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unikey.HandsOnPrjBkEndVerTeamFive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnikeyHandsOnProjectBackEndVersionTeamFiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
